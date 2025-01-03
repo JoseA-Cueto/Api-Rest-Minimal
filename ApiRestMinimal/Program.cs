2b772a448bdc9bee1dd53093182740df8dddbb1d
Miguel David González del Toro <migueldgdt@gmail.com>
2025-01-03 17:05:57 -0500

    refactor (appsetting.json): eliminada cadena de conex

--- ApiRestMinimal/Program.cs
+++ ApiRestMinimal/Program.cs
@@ -10,30 +10,24 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Artículos", Version = "v1" });
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Artï¿½culos", Version = "v1" });
 });
 
-
-
 var app = builder.Build();
-
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Artículos V1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Artï¿½culos V1");
     });
 }
 
-
 ConfigureApiEndpoints(app);
-
 
 app.Run();
 

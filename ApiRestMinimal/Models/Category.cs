44ac3499eb6420112d580fad831fc467c0e904ef
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-02 19:04:34 -0800

    agregar nueva entidad y relacion con Article

---
+++ ApiRestMinimal/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MiApiMinimal.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public List<Article> Articles { get; set; } = new();
+    }
+}
+

04b9019bd21689dea56a766f5448ad4a478048f0
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-04 11:08:09 -0800

    quitar validacion modelo category

--- ApiRestMinimal/Models/Category.cs
+++ ApiRestMinimal/Models/Category.cs
@@ -6,7 +6,7 @@ namespace MiApiMinimal.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; } = null!;
+        public string Name { get; set; } 
         public virtual ICollection<Article> Articles { get; set; }
     }
 }

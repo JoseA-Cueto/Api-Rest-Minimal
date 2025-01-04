688d7c4f7446527e87fcfc5e2dba958eb493c4bc
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-04 10:34:00 -0800

    Agregar Exceptions

---
+++ ApiRestMinimal/Exceptions/Base/ExceptionBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ApiRestMinimal.Exceptions.Base
+{
+    public abstract class ExceptionBase : Exception
+    {
+        public int StatusCode { get; }
+
+        protected ExceptionBase(string message, int statusCode) : base(message)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ ApiRestMinimal/Exceptions/ValidationException.cs
@@ -0,0 +1,9 @@
+﻿using ApiRestMinimal.Exceptions.Base;
+
+namespace MiAplicacion.Exceptions
+{
+    public class ValidationException : ExceptionBase
+    {
+        public ValidationException(string message) : base(message, 400) { }
+    }
+}

231bf3c9d9a17e0b46ded617bc46faacf7d70853
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-05 00:04:18 -0800

    Crear DTOs

---
+++ ApiRestMinimal/DTOs/ArticleDTOs.cs
@@ -0,0 +1,15 @@
+﻿using MiApiMinimal.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiRestMinimal.DTOs
+{
+    public class ArticleDTOs
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Guid CategoryId { get; set; }
+       
+    }
+}

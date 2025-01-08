2c4e3d921161d0bf00fbe828fb4108912daae91d
JoseA-Cueto <joseacm2901@gmail.com>
2025-01-08 02:15:04 -0800

    feat(ImageFile)Agregando Migraciones

--- ApiRestMinimal/Program.cs
+++ ApiRestMinimal/Program.cs
@@ -41,12 +41,12 @@ var builder = WebApplication.CreateBuilder(args);
 
     // Other services
     builder.Services
-        .AddEndpointsApiExplorer()
-        .AddSwaggerGen(options =>
-        {
-            options.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Art�culos", Version = "v1" });
-        });
-    
+        .AddEndpointsApiExplorer();
+    //.AddSwaggerGen(options =>
+    //{
+    //    options.SwaggerDoc("v1", new OpenApiInfo { Title = "API de Art�culos", Version = "v1" });
+    //});
+
     // Cors
     builder.Services.AddCors(options =>
     {
@@ -61,11 +61,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 {
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Art�culos V1"); });
-    }
+    //if (app.Environment.IsDevelopment())
+    //{
+    //    app.UseSwagger();
+    //    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "API de Art�culos V1"); });
+    //}
 
     app.UseCors("AllowAll");
     app.UseMiddleware<ExceptionHandlingMiddleware>();
